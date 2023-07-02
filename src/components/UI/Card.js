89b89b089ff3883b397a9bb06d634b5d9178ea94
Voodoo30fps <voodoo30fps@gmail.com>
2023-07-02 17:24:52 +0300

    card warning fix

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -3,7 +3,7 @@ import './Card.css';
 const Card = (props) => {
   const classes = 'card ' + props.className;
 
-  return <div className={'card ' + props.className}>{props.children}</div>;
+  return <div className={classes}>{props.children}</div>;
 };
 
 export default Card;
